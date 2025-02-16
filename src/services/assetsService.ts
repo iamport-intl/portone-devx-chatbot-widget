81f648e27477b0b101048871c927d83272597a76
Dhairya Tripathi <dhairy@chai.finance>
2025-02-16 18:06:40 +0530

    update: cdn assets url

--- src/services/assetsService.ts
+++ src/services/assetsService.ts
@@ -1,5 +1,5 @@
 const CDN_BASE_URL =
-  "https://cdn.jsdelivr.net/gh/hexdecimal16/chat-widget@main/public";
+  "https://cdn.jsdelivr.net/gh/iamport-intl/chatbot-widget@main/public";
 
 export function getAssetUrl(filename: string): string {
   if (!filename) {
